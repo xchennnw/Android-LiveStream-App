706e4e3b7bd410462917bc7bbe1c82fc7c6f76ae
unknown <xchennnw@gmail.com>
2022-03-06 18:41:45 +0800

    home layout updated

--- app/src/main/java/com/example/livestreamapp/MainActivity.java
+++ app/src/main/java/com/example/livestreamapp/MainActivity.java
@@ -1,39 +1,51 @@
 package com.example.livestreamapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vpager_one;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ViewPager2 viewPager = findViewById(R.id.viewPager);
+        viewPager.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter();
+        viewPager.setAdapter(viewPagerAdapter);
+
         Button btn1 = findViewById(R.id.btn1);
         Button btn2 = findViewById(R.id.btn2);
         Button btn3 = findViewById(R.id.btn3);
-        TextView txt1 = findViewById(R.id.txt1);
+
 
         btn1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                 txt1.setText("展示、写评论");
+
             }
         });
 
-        btn1.setOnClickListener(new View.OnClickListener(){
+        btn2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                txt1.setText("点赞动画");
+
             }
         });
 
-        btn1.setOnClickListener(new View.OnClickListener(){
+        btn3.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                txt1.setText("送礼物");
+
             }
         });
     }
